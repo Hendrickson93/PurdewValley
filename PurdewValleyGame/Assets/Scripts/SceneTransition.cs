51f052f0dd7fde50ef444ec05f0574fdbf4de651
Tyler Cerezo <tylercerezo@yahoo.com>
2022-12-02 15:07:45 -0500

    Added MainMenu (#28)
    Co-authored-by: Hendrickson93 <70722985+Hendrickson93@users.noreply.github.com>

--- PurdewValleyGame/Assets/Scripts/SceneTransition.cs
+++ PurdewValleyGame/Assets/Scripts/SceneTransition.cs
@@ -7,14 +7,14 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
-    public Vector2 playerPosition;
+    public Vector2 playerNextScenePosition;
     public VectorValue playerStorage;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
-            playerStorage.initialValue = playerPosition;
+            playerStorage.initialValue = playerNextScenePosition;
             SceneManager.LoadScene(sceneToLoad);
         }
     }
